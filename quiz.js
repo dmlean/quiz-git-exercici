eed81948642747b1b2bfb22a21b31e7015f71d75
dmlean <dmlean@protonmail.com>
2026-06-07 20:40:44 +0200

    Afegeix dificultat al quiz

--- quiz.js
+++ quiz.js
@@ -2,3 +2,6 @@ const pregunta = "Capital de Catalunya?";
 const resposta = "Barcelona"; 
 console.log(pregunta); 
 console.log("Resposta: " + resposta); 
+ 
+const dificultat = "facil"; 
+console.log("Dificultat: " + dificultat); 
